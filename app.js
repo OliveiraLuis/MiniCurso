0dea4edc86f232b7f8947d7ba99968c8ab41697f
luis.oliveira <oliveira1eduardo2luis3@gmail.com>
2019-05-03 08:27:49 -0300

    Refatoração de código
    Signed-off-by: luis.oliveira <oliveira1eduardo2luis3@gmail.com>

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 // Armazena os clientes conectados
-var clients = {};
+let clients = {};
 
 app.use(express.static('./'));
 app.use(express.static('./src/templates/'));
@@ -12,10 +12,10 @@ app.use(express.static('./src/css'));
 app.use(express.static('./src/js'));
 
 // Dispara a cada vez que um cliente se conecta ao socket
-io.on("connection", function (client) {
+io.on("connection", client => {
 
   // Ao entrar na sala
-  client.on("join", function(name) {
+  client.on("join", name => {
     console.log("Entrou na sala: " + name);
     clients[client.id] = name;
     client.emit("update", "Você se conectou na sala.");
@@ -23,19 +23,19 @@ io.on("connection", function (client) {
   });
 
   // Ao enviar uma mensagem
-  client.on("send", function(msg) {
+  client.on("send", msg => {
     console.log("Messagem: " + msg);
     client.broadcast.emit("chat", clients[client.id], msg);
   });
 
   // Ao sair da sala
-  client.on("disconnect", function() {
+  client.on("disconnect", () => {
     console.log("Desconectou");
     io.emit("update", clients[client.id] + " saiu da sala :(");
     delete clients[client.id];
   });
 });
 
-http.listen(3000, function() {
+http.listen(3000, () => {
   console.log('ouvindo na porta: 3000');
 });
--- src/js/js.js
+++ src/js/js.js
@@ -4,55 +4,75 @@
   var ready = false;
 
   // Ao submeter o fomulário
-  $("#submit").submit((e) => {
-    e.preventDefault();
-    $("#nick").fadeOut();
-    $("#chat").fadeIn();
-    var name = $("#nickname").val();
-    var time = new Date();
-    $("#name").html(name);
-    $("#time").html('First login: ' + time.getHours() + ':' + time.getMinutes());
+  document.getElementById("submit")
+    .addEventListener("submit", (e) => {
+      e.preventDefault();
+      let time = new Date();
+      let name = document.getElementById("nickname").value;
 
-    ready = true;
-    socket.emit("join", name);
-  });
+      document.getElementById("nick").remove();
+      document.getElementById("chat").style.display = "block";
+      document.getElementById("name").innerHTML = name;
+      document.getElementById("time").innerHTML = `First login: ${time.getHours()}:${time.getMinutes()}`;
+
+      ready = true;
+      // Envia evento 'join' pelo socket.io
+      socket.emit("join", name);
+    });
 
   // Quando evento "update" acontecer no servidor
-  socket.on("update", function(msg) {
+  socket.on("update", msg => {
     if (ready) {
-      $('.chat').append('<li class="info">' + msg + '</li>')
+      document.querySelector('.chat')
+        .insertAdjacentHTML('beforeend', `<li class="info">${msg}</li>`);
     }
   });
 
   // Quando usuário apertar enter no campo de mensagem
-  $("#textarea").keypress(function(e) {
-    // Só executa se for teclado "Enter"
-    if(e.which == 13) {
-      var text = $("#textarea").val();
-      var time = new Date();
+  document.getElementById("textarea")
+    .addEventListener("keypress", (e) => {
+      // Só executa se for teclado "Enter"
+      if (e.which !== 13) return;
+
+      let textarea = document.getElementById("textarea");
+      let nickname = document.getElementById("name");
+      let text = textarea.value;
+      let time = new Date();
 
       // Limpa o campo de texto
-      $("#textarea").val('');
+      textarea.value = '';
 
       // Adiciona linha no chat
-      $(".chat").append('<li class="self"><div class="msg"><span>'
-                  + $("#nickname").val() + ':</span>    <p>' + text + '</p><time>' + 
-                  time.getHours() + ':' + time.getMinutes() + '</time></div></li>');
+      document.querySelector(".chat")
+        .insertAdjacentHTML('beforeend',
+          `<li class="self">
+            <div class="msg">
+              <span>${nickname.innerHTML}:</span>
+              <p>${text}</p>
+              <time>${time.getHours()}:${time.getMinutes()}</time>
+            </div>
+          </li>`);
       
-      // Envia a mensagem pelo socket
+      // Envia evento 'send' pelo socket.io
       socket.emit("send", text);
-      // automatically scroll down
+      // 'Scrolla' a página para baixo
       document.getElementById('bottom').scrollIntoView();
-    }
-  });
+    });
 
   // Verifica as mesagens referentes ao chat
-  socket.on("chat", function(client,msg) {
-    if (ready) {
-       var time = new Date();
-       $(".chat").append('<li class="other"><div class="msg"><span>' + 
-                    client + ':</span><p>' + msg + '</p><time>' + time.getHours() + ':' + 
-                    time.getMinutes() + '</time></div></li>');
-    }
+  socket.on("chat", (client, msg) => {
+    if (!ready) return;
+
+    let time = new Date();
+    
+    document.querySelector(".chat")
+    .insertAdjacentHTML('beforeend', 
+        `<li class="other">
+          <div class="msg">
+            <span>${client}:</span>
+            <p>${msg}</p>
+            <time>${time.getHours()}:${time.getMinutes()}</time>
+          </div>
+        </li>`);
    });
 })();
